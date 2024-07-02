239c065000a2e04c34b9a6a00229e5d113381942
At0m1zer <87748468+At0m1zer@users.noreply.github.com>
2024-07-02 06:44:46 +0000

    Changed Int variable name

--- src/Ch00/CodeDemo.cpp
+++ src/Ch00/CodeDemo.cpp
@@ -5,12 +5,12 @@
 #include <iostream>
 
 int main(){
-    float num_1, num_2, result;
+    float num_1, num_3, result;
 
     std::cout << "Enter number 1: " << std::flush;
     std::cin >> num_1;
     std::cout << "Enter number 2: " << std::flush;
-    std::cin >> num_2;
+    std::cin >> num_3;
     
     result = num_1 + num_2;
     
