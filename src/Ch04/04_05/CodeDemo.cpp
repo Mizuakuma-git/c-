fe357532a5af551991d04e47403862891ea479ac
h4sski <114345012+h4sski-programming@users.noreply.github.com>
2024-11-02 21:33:08 +0000

    Done challange 04_05

--- src/Ch04/04_05/CodeDemo.cpp
+++ src/Ch04/04_05/CodeDemo.cpp
@@ -9,6 +9,7 @@
 int main(){
     float GPA = 0.0f;
     int id;
+    int total_credits = 0;
     
     std::vector<Student> students = {Student(1, "George P. Burdell"),
                                     Student(2, "Nancy Rhodes")};
@@ -26,9 +27,38 @@ int main(){
 
     // Calculate the GPA for the selected student.
     // Write your code here
+    for (Grade& grade : grades){
+        if (grade.get_student_id() == id){
+            int credit = 0;
+            for (auto course : courses){
+                if (grade.get_course_id() == course.get_id())
+                    credit = course.get_credits();
+            };
+            total_credits += credit;
+            switch (grade.get_grade()){
+                case 'A':
+                    GPA += 4*credit;
+                    break;
+                case 'B':
+                    GPA += 3*credit;
+                    break;
+                case 'C':
+                    GPA += 2*credit;
+                    break;
+                case 'D':
+                    GPA += 1*credit;
+                    break;
+                default:
+                    GPA += 0*credit;
+                    break;
+            };
+        };
+    };
+
+    GPA /= total_credits;
 
     std::string student_str;
-    student_str = students[0].get_name(); // Change this to the selected student's name
+    student_str = students[id-1].get_name(); // Change this to the selected student's name
 
     std::cout << "The GPA for " << student_str << " is " << GPA << std::endl;
     
